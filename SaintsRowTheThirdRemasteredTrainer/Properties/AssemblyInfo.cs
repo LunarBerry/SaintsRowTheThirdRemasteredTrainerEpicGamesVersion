cd26eef83eeb42f6a679c5c46ccaab376715429e
Lunar Berry <marcel.kalff@outlook.de>
2021-09-15 17:31:59 +0200

    v2.3 first Release for public

--- SaintsRowTheThirdRemasteredTrainer/Properties/AssemblyInfo.cs
+++ SaintsRowTheThirdRemasteredTrainer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("SaintsRowTheThirdRemasteredTrainer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("a Hack/Cheat Trainer to get Max XP, GodMode, Unlimited Ammo...")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SaintsRowTheThirdRemasteredTrainer")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Lunar Berry Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
