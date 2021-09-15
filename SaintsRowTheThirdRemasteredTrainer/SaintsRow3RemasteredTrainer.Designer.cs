7c7e8ce56ffb6105fac19f422b081cb0d44c0212
marce <marce@DESKTOP-NCM11FB>
2021-09-15 17:12:24 +0200

    Projektdateien hinzufügen.

---
+++ SaintsRowTheThirdRemasteredTrainer/SaintsRow3RemasteredTrainer.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace SaintsRowTheThirdRemasteredTrainer
+{
+    partial class SaintsRow3RemasteredTrainer
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SaintsRow3RemasteredTrainer));
+            this.addMoneyButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addMoneyButton
+            // 
+            this.addMoneyButton.BackColor = System.Drawing.Color.Indigo;
+            this.addMoneyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addMoneyButton.ForeColor = System.Drawing.Color.White;
+            this.addMoneyButton.Location = new System.Drawing.Point(124, 32);
+            this.addMoneyButton.Name = "addMoneyButton";
+            this.addMoneyButton.Size = new System.Drawing.Size(96, 26);
+            this.addMoneyButton.TabIndex = 0;
+            this.addMoneyButton.Text = "Betrag setzen";
+            this.addMoneyButton.UseVisualStyleBackColor = false;
+            this.addMoneyButton.Click += new System.EventHandler(this.addMoneyButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(44, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Saints Row The Third Remastered nicht offen";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(9, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(57, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::SaintsRowTheThirdRemasteredTrainer.Properties.Resources.kisspng_saints_row_the_third_saints_row_iv_saints_row_2_s_row_5b355f968166b7_79281911153022453453;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(252, 254);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.addMoneyButton);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.ForeColor = System.Drawing.Color.Magenta;
+            this.groupBox1.Location = new System.Drawing.Point(345, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 70);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bargeld";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Max 10000000: 1 und 7x die 0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox2.ForeColor = System.Drawing.Color.Magenta;
+            this.groupBox2.Location = new System.Drawing.Point(345, 109);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(233, 59);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Unendlich Munition";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(83, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Munition";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox3.ForeColor = System.Drawing.Color.Magenta;
+            this.groupBox3.Location = new System.Drawing.Point(345, 174);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(233, 59);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "God Mode";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(81, 23);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(73, 17);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "GodMode";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox4.ForeColor = System.Drawing.Color.Magenta;
+            this.groupBox4.Location = new System.Drawing.Point(345, 239);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(233, 56);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Max XP/EP";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Indigo;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(61, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Max XP/EP";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SaintsRow3RemasteredTrainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(654, 356);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SaintsRow3RemasteredTrainer";
+            this.Text = "Saints Row The Third Remastered Trainer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addMoneyButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SaintsRowTheThirdRemasteredTrainer/SaintsRow3RemasteredTrainer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Memory;
+
+namespace SaintsRowTheThirdRemasteredTrainer
+{
+    public partial class SaintsRow3RemasteredTrainer : Form
+    {
+        Mem m = new Mem();
+
+
+        public SaintsRow3RemasteredTrainer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            int PID = m.GetProcIdFromName("SRTTR");
+            if (PID > 0)
+            {
+                m.OpenProcess(PID);
+                label1.ForeColor = Color.Green;
+                label1.Text = "Saints Row The Third Remastered Gefunden!";
+                label2.ForeColor = Color.Green;
+                label2.Text = "Hackfunktionen erfolgreich geladen!";
+
+                Thread AMM = new Thread(unlAmmo);
+                AMM.Start();
+
+                Thread UH = new Thread(unHealth);
+                UH.Start();
+            }
+            else
+            {
+                MessageBox.Show("Spiel nicht gefunden Blyat. Hack schließen und das Spiel als erstes starten.");
+            }
+        }
+
+        private void addMoneyButton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                m.WriteMemory("SRTTR.exe+0x11E9F20,1E90", "int", textBox1.Text + "00");
+
+            }
+
+            /*
+             * alle offset möglichkeiten:
+             * SRTTR.exe+11E9F20,1E90
+             * SRTTR.exe+1A588F8,1E90
+             * SRTTR.exe+1E22A10,1E90
+             * SRTTR.exe+1E3F370,1E90
+             * SRTTR.exe+254B710,1E90
+             * SRTTR.exe+2884028,1E90
+             */
+        }
+
+        private void unlAmmo()
+        {
+            while (true)
+            {
+                if (checkBox1.Checked)
+                {
+                    m.WriteMemory("SRTTR.exe+0x1A0C0E8,204", "int", "40");
+                    Thread.Sleep(50);
+                }
+            }
+            /*
+             * 1A8B812CD78,204
+             * 1A8B8087DD8,204
+             * 1A8B7AF6038,204
+             * 1A8B7A6C1A0,204
+             * 1A8B7A3B1D8,204
+             */
+        }
+
+        private void unHealth()
+        {
+            while (true)
+            {
+                if (checkBox2.Checked)
+                {
+                    m.WriteMemory("SRTTR.exe+0x1E22A10,1EA8", "int", "1148846080");
+                    Thread.Sleep(50);
+                }
+            }
+            /*
+             * SRTTR.exe+0x1A23980,1EA8
+             * SRTTR.exe+1A588F8,1EA8
+             * SRTTR.exe+1E22A10,1EA8
+             * SRTTR.exe+1E26668,1EA8
+             * SRTTR.exe+1E3F370,1EA8
+             * SRTTR.exe+20538F8,1EA8
+             * SRTTR.exe+254B7F0,1EA8
+             * SRTTR.exe+26C4A40,1EA8
+             * SRTTR.exe+2884028,1EA8
+             */
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m.WriteMemory("SRTTR.exe+0x1E22A10,2054", "int", "255500");
+            MessageBox.Show("Deine XP/EP wurden gesetzt. Steig in ein Straßenfahrzeug und mache knappe überholmanöver. Anschließend stehen bleiben und 3-10 Sekunden warten bis die XP/EP gezählt werden.");
+
+            /*
+             * SRTTR.exe+1E22A10,2054
+             * SRTTR.exe+026C4A58,178
+             * SRTTR.exe+026C4A58
+             */
+        }
+    }
+}
