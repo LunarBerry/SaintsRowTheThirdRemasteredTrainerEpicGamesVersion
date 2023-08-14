68af4f32a05000556f621714e1d42afac99c5980
Lunar Berry <marcel.kalff@outlook.de>
2023-08-14 19:37:49 +0200

    UPDATE 2 (all hacks working) +Unlimited Health + Max XP

--- SaintsRowTheThirdRemasteredTrainer/SaintsRow3RemasteredTrainer.cs
+++ SaintsRowTheThirdRemasteredTrainer/SaintsRow3RemasteredTrainer.cs
@@ -82,32 +82,35 @@ namespace SaintsRowTheThirdRemasteredTrainer
             {
                 if (checkBox2.Checked)
                 {
-                    m.WriteMemory("SRTTR.exe+0x1E22A10,1EA8", "int", "1148846080");
-                    Thread.Sleep(50);
+                    m.WriteMemory("SRTTR.exe+0x11EAF30,1EA8", "int", "1148846080");
+                    Thread.Sleep(10);
                 }
             }
             /*
-             * SRTTR.exe+0x1A23980,1EA8
-             * SRTTR.exe+1A588F8,1EA8
-             * SRTTR.exe+1E22A10,1EA8
-             * SRTTR.exe+1E26668,1EA8
-             * SRTTR.exe+1E3F370,1EA8
-             * SRTTR.exe+20538F8,1EA8
-             * SRTTR.exe+254B7F0,1EA8
-             * SRTTR.exe+26C4A40,1EA8
-             * SRTTR.exe+2884028,1EA8
+             * SRTTR.exe+0x11EAF30,1EA8
+             * SRTTR.exe+0x1A24980,1EA8
+             * SRTTR.exe+0x1A598F8,1EA8
+             * SRTTR.exe+0x1E21490,1EA8
+             * SRTTR.exe+0x1E3DDF0,1EA8
+             * SRTTR.exe+0x2052398,1EA8
+             * SRTTR.exe+0x26CF328,1EA8
+             * SRTTR.exe+0x28C8650,1EA8
              */
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            m.WriteMemory("SRTTR.exe+026C4A58,178", "int", "255500");
-            MessageBox.Show("Deine XP/EP wurden gesetzt. Steig in ein Straßenfahrzeug und mache knappe überholmanöver. Anschließend stehen bleiben und 3-10 Sekunden warten bis die XP/EP gezählt werden.");
+            m.WriteMemory("SRTTR.exe+0x11EAF30,2054", "int", "255500");
+            MessageBox.Show("Get ingame XP to trigger this hack. headshot on enemy or something");
 
             /*
-             * SRTTR.exe+1E22A10,2054
-             * SRTTR.exe+026C4A58,178
-             * SRTTR.exe+026C4A58
+             * SRTTR.exe+0x11EAF30,2054
+             * SRTTR.exe+0x1A24980,2054
+             * SRTTR.exe+0x1A598F8,2054
+             * SRTTR.exe+0x1E21490,2054
+             * SRTTR.exe+0x1E3DDF0,2054
+             * SRTTR.exe+0x2052398,2054
+             * SRTTR.exe+0x26CF328,2054
              */
         }
     }
